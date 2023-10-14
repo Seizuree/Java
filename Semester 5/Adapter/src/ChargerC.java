ca77ecdf144efb5e7c113582e92a1ae879228519
Seizuree <briangiovanni03@gmail.com>
2023-10-15 00:36:12 +0700

    5th semester projects

---
+++ Semester 5/Adapter/src/ChargerC.java
@@ -0,0 +1,14 @@
+public class ChargerC {
+
+	private int output;
+	
+	public ChargerC() {
+		// TODO Auto-generated constructor stub
+		this.output = 20;
+	}
+
+	public int getOutput() {
+		return output;
+	}
+
+}
